062eed5d5ae7438f05b4a8af6ab32b662c165c0c
Samuel <samrodri44@gmail.com>
2026-01-29 22:08:44 -0500

    implemented websocket connection function

--- api.py
+++ api.py
@@ -1,17 +1,18 @@
+import asyncio
+import json
 import os
 
 import requests
+import websockets
 from dotenv import load_dotenv
 
 load_dotenv()
 BASE_URL = os.getenv("BASE_URL_PANEL")
 SERVER_ID = os.getenv("SERVER")
 API_KEY = os.getenv("API_KEY")
-jwt_token = None
-socket_url = None
 
 
-def GetJWT():
+def get_jwt():
     headers = {
         "Authorization": f"Bearer {API_KEY}",
         "Accept": "Application/vnd.pterodactyl.v1+json",
@@ -29,5 +30,35 @@ def GetJWT():
     print(f"Websocket URL: {socket_url}")
     print(f"JWT Token: {jwt_token}")
 
+    return socket_url, jwt_token
 
-GetJWT()
+
+async def connect_websocket():
+    socket_url, token = get_jwt()
+
+    additional_headers = {
+        "Authorization": f"Bearer {token}",
+        "Origin": f"{BASE_URL}",
+    }
+
+    async with websockets.connect(
+        socket_url, additional_headers=additional_headers
+    ) as websocket:
+        print("Websocket Connection Established.")
+
+        # Authenticate
+        auth_message = {
+            "event": "auth",
+            "args": [token],
+        }
+        await websocket.send(json.dumps(auth_message))
+
+        # Listen for messages
+        async for message in websocket:
+            data = json.loads(message)
+            print("Received:", data)
+
+
+asyncio.run(connect_websocket())
+
+connect_websocket()
