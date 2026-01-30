2ef862a0a1091783a5c18a5af0f33469586a7961
Samuel <samrodri44@gmail.com>
2026-01-30 18:50:07 -0500

    created a class to manage websocket connection

--- api.py
+++ api.py
@@ -12,53 +12,60 @@ SERVER_ID = os.getenv("SERVER")
 API_KEY = os.getenv("API_KEY")
 
 
-def get_jwt():
-    headers = {
-        "Authorization": f"Bearer {API_KEY}",
-        "Accept": "Application/vnd.pterodactyl.v1+json",
-    }
+class PterodactylWS:
+    def __init__(self):
+        self.ws_url = None
+        self.origin = None
+        self.ws = None
+        self.queue = asyncio.Queue()
+        self.task = None
 
-    response = requests.get(
-        f"{BASE_URL}/api/client/servers/{SERVER_ID}/websocket", headers=headers
-    )
-
-    print(response)
-    data = response.json()["data"]
-    socket_url = data["socket"]
-    jwt_token = data["token"]
-
-    print(f"Websocket URL: {socket_url}")
-    print(f"JWT Token: {jwt_token}")
-
-    return socket_url, jwt_token
-
-
-async def connect_websocket():
-    socket_url, token = get_jwt()
-
-    additional_headers = {
-        "Authorization": f"Bearer {token}",
-        "Origin": f"{BASE_URL}",
-    }
-
-    async with websockets.connect(
-        socket_url, additional_headers=additional_headers
-    ) as websocket:
-        print("Websocket Connection Established.")
-
-        # Authenticate
-        auth_message = {
-            "event": "auth",
-            "args": [token],
+    def get_jwt(self):
+        headers = {
+            "Authorization": f"Bearer {API_KEY}",
+            "Accept": "Application/vnd.pterodactyl.v1+json",
         }
-        await websocket.send(json.dumps(auth_message))
 
-        # Listen for messages
-        async for message in websocket:
-            data = json.loads(message)
+        response = requests.get(
+            f"{BASE_URL}/api/client/servers/{SERVER_ID}/websocket", headers=headers
+        )
+
+        print(response)
+        data = response.json()["data"]
+        socket_url = data["socket"]
+        jwt_token = data["token"]
+
+        print(f"Websocket URL: {socket_url}")
+        print(f"JWT Token: {jwt_token}")
+
+        return socket_url, jwt_token
+
+    async def connect(self):
+        socket_url, token = self.get_jwt()
+
+        additional_headers = {
+            "Authorization": f"Bearer {token}",
+            "Origin": f"{BASE_URL}",
+        }
+
+        async with websockets.connect(
+            socket_url, additional_headers=additional_headers
+        ) as websocket:
+            print("Websocket Connection Established.")
+
+            # Authenticate
+            auth_message = {
+                "event": "auth",
+                "args": [token],
+            }
+            await websocket.send(json.dumps(auth_message))
+
+            # Listen for messages
+            async for message in websocket:
+                data = json.loads(message)
             print("Received:", data)
 
 
-asyncio.run(connect_websocket())
-
-connect_websocket()
+# May not be necessary
+# if __name__ == "__main__":
+#    pterows_instance = PterodactylWS()
