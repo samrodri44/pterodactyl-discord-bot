2cbdb40f1537a24c514abe7a522556a434ceb41a
Samuel <samrodri44@gmail.com>
2026-01-31 23:53:39 -0500

    moved command prefix to environment file

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
 dev = os.getenv("DEVELOPER")
 member_role = os.getenv("MEMBER_ROLE")
+prefix = os.getenv("PREFIX")
 
 handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(command_prefix="mc!", intents=intents)
+bot = commands.Bot(command_prefix=f"{prefix}", intents=intents)
 ws_manager = PterodactylWS()
 
 
