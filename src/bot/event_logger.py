c82501fbcf14911ed44e33e752c3bf9d41605f70
Samuel Rodriguez <samrodri44@gmail.com>
2026-05-28 16:37:36 -0400

    Setup file handler and formatter

--- src/bot/event_logger.py
+++ src/bot/event_logger.py
@@ -1,6 +1,8 @@
 import logging
 
 logger = logging.getLogger(__name__)
+handler = logging.handlers.RotatingFileHandler(filename="discord.log", encoding="utf-8", mode="w", maxBytes=5_000_000, backupCount=3)
+formatter = logging.Formatter(fmt="%(asctime)s | %(levelname)-8s | %(message)s", datefmt="%Y-%m-%d %H:%M:%S", style="%")
 
 class EventLogger:
     def __init__(self):
