aa133366061848a61c6a48336270acfa83019691
Samuel Rodriguez <samrodri44@gmail.com>
2026-06-22 21:32:46 -0400

    Attached handler and formatter to logger and enabled testing

--- src/bot/event_logger.py
+++ src/bot/event_logger.py
@@ -1,17 +1,27 @@
 import logging
+from logging.handlers import RotatingFileHandler
 
 logger = logging.getLogger(__name__)
-handler = logging.handlers.RotatingFileHandler(filename="discord.log", encoding="utf-8", mode="w", maxBytes=5_000_000, backupCount=3)
+handler = RotatingFileHandler(filename="events.log", encoding="utf-8", mode="w", maxBytes=5_000_000, backupCount=3)
 formatter = logging.Formatter(fmt="%(asctime)s | %(levelname)-8s | %(message)s", datefmt="%Y-%m-%d %H:%M:%S", style="%")
 
 class EventLogger:
     def __init__(self):
-        logging.basicConfig(filename="events.log", level=logging.INFO)
+        logger.setLevel(logging.INFO)
         logger.info("Starting logger")
+        logger.info("Attaching formatter to handler")
+        handler.setFormatter(formatter)
+        logger.info("Adding handler...")
+        logger.addHandler(handler)
 
     def run(self, queue):
-        try:
-            while(True):
-        except Exception:
-            print("logger error: {e}")
+        #try:
+        #    while(True):
+        #except Exception:
+        #    print("logger error: {e}")
+        logger.info("running...")
+        logger.info("what is going on?")
 
+if __name__ == "__main__":
+    el = EventLogger()
+    el.run(1)
